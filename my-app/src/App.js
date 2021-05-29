72ca196d77dca0171b15d56df7d1b07308fbc523
Huang Daoxu <34485325+sanjayheaven@users.noreply.github.com>
2021-05-29 13:23:33 +0800

    布局

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,26 +1,19 @@
-import React, { useState } from "react";
+import React, { useState } from "react"
 // import logo from "./logo.svg";
 // import "./App.css";
-import "antd/dist/antd.css"; // 引入ant design 样式
+import "antd/dist/antd.css" // 引入ant design 样式
 // import WelcomeFunc from "./components/welcomeFunc.js";
 // import WelcomeClass from "./components/welcomeClass.js";
 
 // 语言配置
-import zhCN from "antd/lib/locale/zh_CN";
-import { Button, ConfigProvider } from "antd";
+import zhCN from "antd/lib/locale/zh_CN"
+import { Button, ConfigProvider } from "antd"
 
 // 基本布局
-import BasicLayout from "./layouts";
-
-// 布局
-// vue的时候看到过这种布局，当时引入没用起来。
-// 需要去看去理解这种布局 以后直接用这个布局
-import ProLayout, { PageContainer } from "@ant-design/pro-layout";
-// 引入content组件
-// import WelcomeClass from "./components/welcomeClass.js";
+import BasicLayout from "./layouts"
 
 // 路由菜单配置引入
-import routes from "./routes/index";
+import routes from "./routes/index"
 export default () => {
   return (
     <ConfigProvider locale={zhCN}>
@@ -34,5 +27,5 @@ export default () => {
         </BasicLayout>
       </div>
     </ConfigProvider>
-  );
-};
+  )
+}
--- my-app/src/layouts/index.js
+++ my-app/src/layouts/index.js
@@ -1,25 +1,66 @@
-import React from "react";
-import "./index.css";
-import { Layout, Menu, Breadcrumb } from "antd";
-const { Header, Content, Footer } = Layout;
+import React, { useState } from "react"
+import "./index.css"
+import { Layout, Menu } from "antd"
+const { Header, Content, Footer, Sider } = Layout
+import {
+  MenuUnfoldOutlined,
+  MenuFoldOutlined,
+  UserOutlined,
+  VideoCameraOutlined,
+  UploadOutlined,
+} from "@ant-design/icons"
+
+const FooterNew = () => {
+  return (
+    <Footer style={{ textAlign: "center" }}>
+      React 版后台 ©2021 Created by Sanjay
+    </Footer>
+  )
+}
+
+const HeaderNew = ({ collapsed, setCollapsed }) => {
+  return (
+    <Header className="site-layout-header" style={{ padding: 0 }}>
+      {React.createElement(collapsed ? MenuUnfoldOutlined : MenuFoldOutlined, {
+        className: "trigger",
+        onClick: setCollapsed,
+      })}
+    </Header>
+  )
+}
+
+const SiderMenu = ({ collapsed = false }) => {
+  return (
+    <Sider trigger={null} collapsible collapsed={collapsed}>
+      <div className="logo">Sanjay</div>
+      <Menu theme="dark" mode="inline" defaultSelectedKeys={["1"]}>
+        <Menu.Item key="1" icon={<UserOutlined />}>
+          nav 1
+        </Menu.Item>
+        <Menu.Item key="2" icon={<VideoCameraOutlined />}>
+          nav 2
+        </Menu.Item>
+        <Menu.Item key="3" icon={<UploadOutlined />}>
+          nav 3
+        </Menu.Item>
+      </Menu>
+    </Sider>
+  )
+}
 
 export default (props) => {
+  const [collapsed, setCollapsed] = useState(false)
   return (
-    <Layout>
-      <Header>
-        <div className="logo" />
-        <Menu theme="dark" mode="horizontal" defaultSelectedKeys={["2"]}>
-          <Menu.Item key="1">nav 1</Menu.Item>
-          <Menu.Item key="2">nav 2</Menu.Item>
-          <Menu.Item key="3">nav 3</Menu.Item>
-        </Menu>
-      </Header>
-      <Content style={{ padding: "0 50px" }}>
-        <div className="layout-content">{props.children}</div>
-      </Content>
-      <Footer style={{ textAlign: "center" }}>
-        React 版后台 ©2021 Created by Sanjay
-      </Footer>
+    <Layout className="layout">
+      <SiderMenu collapsed={collapsed} />
+      <Layout className="site-layout">
+        <HeaderNew
+          collapsed={collapsed}
+          setCollapsed={() => setCollapsed(!collapsed)}
+        />
+        <Content className="site-layout-content">{props.children}</Content>
+        <FooterNew />
+      </Layout>
     </Layout>
-  );
-};
+  )
+}
